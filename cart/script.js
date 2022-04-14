012d5710567e3d9600495f42688375e4d32f318e
ostrekodowanie <100235977+ostrekodowanie@users.noreply.github.com>
2022-04-14 16:43:18 +0200

    Add files via upload

---
+++ cart/script.js
@@ -0,0 +1,29 @@
+//variables
+const closeX = document.querySelector('.x')
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.navbar')
+
+// functions
+
+const hideNavbar = () => {
+    nav.classList.remove('nav-active')
+    closeX.classList.remove('x-active')
+}
+
+const navHeight = () => {
+    let screenHeight = window.innerHeight
+    nav.style.height = screenHeight + "px";
+}
+
+const navMove = () => {
+    burger.addEventListener('click', () => {
+        nav.classList.add('nav-active')
+        closeX.classList.add('x-active')
+        navHeight();
+    })
+    closeX.addEventListener('click', () => {
+        hideNavbar();
+    })
+}
+
+navMove();
