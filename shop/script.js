b3c2ab3cbe2647486f011de55dea6a24647dda1d
ostrekodowanie <100235977+ostrekodowanie@users.noreply.github.com>
2022-04-19 03:11:35 +0200

    Update script.js

--- shop/script.js
+++ shop/script.js
@@ -1,9 +1,7 @@
-//variables
+//mobile navbar
 const closeX = document.querySelector('.x')
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.navbar')
-
-// functions
 
 const hideNavbar = () => {
     nav.classList.remove('nav-active')
@@ -26,4 +24,45 @@ const navMove = () => {
     })
 }
 
-navMove();
+//appending or removing html paragraph for each of the stock divs
+
+const stock = document.querySelectorAll('.stock')
+const stockParagraph = document.querySelectorAll('.stock p')
+const stockTextDiv = document.querySelectorAll('.stock-text')
+
+
+const appendToStockText = () => {
+    let screenWidth = window.innerWidth
+    if(screenWidth >= 1400) {
+        for(let i = 0; i < stock.length; i++) {
+            stockTextDiv[i].append(stockParagraph[i])
+        }
+    }
+}
+
+const removeFromStockText = () => {
+    let screenWidth = window.innerWidth
+    let putBefore = document.querySelectorAll('.stock a')
+    if(screenWidth < 1400) {
+        for(let i = 0; i < stock.length; i++) {
+            stock[i].insertBefore(stockParagraph[i], putBefore[i])
+        }
+    }
+}
+
+
+//calling functions based on the screen width
+
+window.addEventListener('resize', () => {
+    let screenWidth = window.innerWidth
+    if(screenWidth < 750) navMove()
+        else nav.style.height = "6rem"
+    appendToStockText()
+    removeFromStockText()
+})
+
+//since css is mobile-first, calling functions which require default setup
+
+navMove()
+appendToStockText()
+
